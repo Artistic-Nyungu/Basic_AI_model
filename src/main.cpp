b63fa3ae782272d8b038352ce5ad0fd65bd35aef
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2025-03-26 13:22:40 +0200

    Adding IMGUI and GLFW... ( Hello World )

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 // Hyperparameters
 const int NODES_PER_LAYER[] = {784, 6, 4, 6, 10};
@@ -11,7 +16,7 @@ const int MAX_ITERATIONS = 500;
 /// length - the length of the array
 /// returns - a float representing the magnitude
 float magnitude(const float* arr, int length){
-    float squareSum = 0.0;
+    float squareSum = 0.0f;
     for (int i=0; i<length; i++) {
         squareSum += arr[i] * arr[i];
     }
@@ -32,31 +37,130 @@ float* normalize(const float* arr, int length){
     return normalized;
 }
 
+struct range{
+    int begin;
+    int end;
+};
+
+struct forwardingLayer{
+    range nodes;
+    range weights;
+    range biases;
+};
+
 int main() {
-    int _nNodes, _nWeights, _nBiases = 0;
+    // Initialize library
+    if(!glfwInit())
+    {
+        std::cerr << "GLFW could not start\n";
+        exit(-1);  
+    }
+
+    // Instantiate window
+    auto _window = glfwCreateWindow(640, 480, "OpenGL Programming Application", NULL, NULL);
+    if(!_window)
+    {
+        std::cerr << "Failed to create window\n";
+        glfwTerminate();
+        exit(-1);
+    }
+    
+    // Make window's context current, has to be before loading glad
+    glfwMakeContextCurrent(_window);
+    
+    // Initialize ImGUI
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(_window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
+
+    // Set callback function for when the window size is changed
+    glfwSetFramebufferSizeCallback(_window, [](GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    });
+
+
+    // NN
+    int _nNodes = 0, _nWeights = 0, _nBiases = 0;
     int nplLength = sizeof(NODES_PER_LAYER)/sizeof(int);
+    forwardingLayer* _forwardingLayers = new forwardingLayer[nplLength - 1];
     for(int i=0; i < nplLength; i++){
         _nNodes += NODES_PER_LAYER[i];  // Count all the nodes
         if(i < nplLength - 1){
             _nWeights += NODES_PER_LAYER[i] * NODES_PER_LAYER[i+1]; // Count all the weights
             _nBiases += NODES_PER_LAYER[i+1]; // Count all the biases
+            // Add ranges for forwarding layers
+            // Layer nodes end() = sum of npy(i) from i=0 to i=current layer, begin() = end() - npy(current layer)
+            // Weights end() = sum of ( npy(i) * npy(i+1) ) from i=0 to i=current layer, i < nLayers , begin() = end() - npy(current layer) * npy(current layer + 1)
+            // Biases end() = sum of npy(i+1) from i=0 to i=current layer, i < nLayers, begin() = end() - npy(current layer + 1)
+            _forwardingLayers[i] = {
+                {_nNodes, _nNodes - NODES_PER_LAYER[i]},
+                {_nWeights, _nWeights - NODES_PER_LAYER[i] * NODES_PER_LAYER[i+1]},
+                {_nBiases, _nBiases - NODES_PER_LAYER[i+1]}
+            };
         }
     }
+
 
     // All layer features will be mapped to a 1D array
     // Features of one layer will be sequential until the nth of the layer,
     // then the next will belong to the following layer
     float* _nodes = new float[_nNodes]; 
     float* _weights = new float[_nWeights];
-    float* _biases = new float[_nBiases];
+    float* _biases = new float[_nBiases]{0};
 
-    std::cout << "Hello World!" << std::endl;
+    // Initialize weights to random values between -5 & 5
+    for(int i=0; i<_nWeights; i++)
+        _weights[i] = static_cast<float>(rand()) / RAND_MAX * 10.0f - 5.0f;
+
+    ImVec4 clearColor = {};
+    while(!glfwWindowShouldClose(_window))
+    {
+        glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplGlfw_NewFrame();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui::NewFrame();
+
+        ImVec2 pos = ImGui::GetCursorScreenPos();
+        ImVec2 wSize = ImGui::GetContentRegionAvail();
+
+        glViewport(pos.x, pos.y, wSize.x, wSize.y);
+
+        // Rendering commands here
+        ImGui::Begin("Basic AI Model");
+            ImGui::Text("Hello World");
+        ImGui::End();
+
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(_window);
+
+        glfwPollEvents();
+    }
 
     // Clean
     delete[] _nodes;
     delete[] _weights;
     delete[] _biases;
+    delete[] _forwardingLayers;
     _nodes = _weights = _biases = nullptr;
+    _forwardingLayers = nullptr;
+
+    // Clean glfw
+    ImGui_ImplGlfw_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
 
     return 0;
 }
